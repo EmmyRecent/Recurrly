a8b715170bc7fb694c3cc2ffee5df7f7e981313c
EmmyRecent <chukwuemekaeziagulu@gmail.com>
2026-03-30 22:49:48 +0100

    feat(app): add fonts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -8,33 +8,27 @@ export const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="bg-background flex-1 p-5">
-      <Text className="text-success rounded-4xl bg-red-50 p-4 text-xl font-bold">
-        Welcome to Native wind!
+      <Text className="text-primary font-sans-bold rounded-4xl p-4 text-5xl">
+        Home
       </Text>
 
       <Link
         href="/onboarding"
-        className="bg-primary mt-4 rounded p-4 text-white"
+        className="bg-primary font-sans-bold mt-4 rounded p-4 text-white"
       >
         Go to Onboarding
       </Link>
       <Link
         href="/(auth)/sign-in"
-        className="bg-primary mt-4 rounded p-4 text-white"
+        className="bg-primary font-sans-bold mt-4 rounded p-4 text-white"
       >
         Go to sign in{" "}
       </Link>
       <Link
         href="/(auth)/sign-up"
-        className="bg-primary mt-4 rounded p-4 text-white"
+        className="bg-primary font-sans-bold mt-4 rounded p-4 text-white"
       >
         Go to sign up{" "}
-      </Link>
-
-      <Link
-        href={{ pathname: "/subscriptions/[id]", params: { id: "claude" } }}
-      >
-        Claude pro subscription
       </Link>
     </SafeAreaView>
   );
